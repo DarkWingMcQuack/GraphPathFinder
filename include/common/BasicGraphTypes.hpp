7b7d3f9afcee87ae89a8b7a0b5641c79c6910f59
lukas <np-hardass@protonmail.com>
2021-08-25 23:59:14 +0200

    make all the types arithmetic

--- include/common/BasicGraphTypes.hpp
+++ include/common/BasicGraphTypes.hpp
@@ -8,12 +8,12 @@
 namespace common {
 
 // clang-format off
-using NodeID     = fluent::NamedType<std::size_t, struct NodeIDTag>;
-using EdgeID     = fluent::NamedType<std::size_t, struct EdgeIDTag>;
-using Weight     = fluent::NamedType<std::int_fast64_t, struct WeightTag>;
-using NodeLevel  = fluent::NamedType<std::int_fast64_t, struct NodeLevelTag>;
-using Latitude   = fluent::NamedType<double, struct LatitudeTag>;
-using Longitude  = fluent::NamedType<double, struct LongitudeTag>;
+using NodeID     = fluent::NamedType<std::size_t, struct NodeIDTag, fluent::Arithmetic>;
+using EdgeID     = fluent::NamedType<std::size_t, struct EdgeIDTag, fluent::Arithmetic>;
+using NodeLevel  = fluent::NamedType<std::size_t, struct NodeLevelTag, fluent::Arithmetic>;
+using Weight     = fluent::NamedType<std::int_fast64_t, struct WeightTag, fluent::Arithmetic>;
+using Latitude   = fluent::NamedType<double, struct LatitudeTag, fluent::Arithmetic>;
+using Longitude  = fluent::NamedType<double, struct LongitudeTag, fluent::Arithmetic>;
 // clang-format on
 
 } // namespace common
