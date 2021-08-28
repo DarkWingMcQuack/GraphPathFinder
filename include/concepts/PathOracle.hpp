95f1bf5103ac0f4bf71b3d82a75698058b9b32f5
lukas <np-hardass@protonmail.com>
2021-08-28 12:40:47 +0200

    change the path oracle

--- include/concepts/PathOracle.hpp
+++ include/concepts/PathOracle.hpp
@@ -3,6 +3,7 @@
 #include <concepts/EdgeWeights.hpp>
 #include <concepts/Nodes.hpp>
 #include <concepts/Path.hpp>
+#include <graphs/Path.hpp>
 #include <optional>
 
 namespace concepts {
@@ -11,12 +12,12 @@ namespace concepts {
 
 //O is a distance oracle for G
 template<typename O>
-concept PathOracle = requires(const O& oracle, common::NodeID src, common::NodeID trg)
+concept PathOracle = requires(O& oracle, common::NodeID src, common::NodeID trg)
 {
 	/**
 	 * @returns an optional contaning the shortest path between src and trg, std::nullopt if no such path exists
 	 */
-    {oracle.pathBetween(src, trg)} noexcept -> PathOpt;
+    {oracle.pathBetween(src, trg)} noexcept -> std::same_as<std::optional<graphs::Path>>;
 };
 // clang-format on
 
