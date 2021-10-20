b1058aa813c1ef2f0c3c622fb8824c71ff8fd810
lukas <np-hardass@protonmail.com>
2021-10-20 17:45:12 +0200

    added concept for onetomany oracles

--- include/concepts/DistanceOracle.hpp
+++ include/concepts/DistanceOracle.hpp
@@ -16,9 +16,24 @@ concept DistanceOracle = requires(O& oracle, common::NodeID src, common::NodeID 
 	 * @returns the shortest path distance between the nodes represented by the node ids given
 	 * the shortest path distance musst be calculated by cost of the path starting with the source node
 	 * and ending at the target node.
-	 * if no path between src and trg exists std::nullopt musst be returned
+	 * if no path between src and trg exists INFINITY_WEIGHT musst be returned
 	 */
     {oracle.distanceBetween(src, trg)} noexcept -> std::same_as<common::Weight>;
+
+	/**
+	 * @returns true if the distance oracle can be used in a multithreaded environment,
+	 * false otherwise
+	 */
+	{O::is_threadsafe} noexcept -> std::common_with<const bool>;
+};
+
+template<typename O>
+concept OneToManyDistanceOracle = requires(O& oracle, common::NodeID src)
+{
+	/**
+	 * @returns a vector of distances to all other nodes
+	 */
+    {oracle.distancesFrom(src)} noexcept -> std::same_as<const std::vector<common::Weight>&>;
 
 	/**
 	 * @returns true if the distance oracle can be used in a multithreaded environment,
