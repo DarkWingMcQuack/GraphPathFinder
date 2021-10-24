cddbe8b4d1b299512a6abcf36920c39cb588c16c
lukas <np-hardass@protonmail.com>
2021-10-25 00:53:51 +0200

    some refactoring

--- include/algorithms/distoracle/PHAST.hpp
+++ include/algorithms/distoracle/PHAST.hpp
@@ -58,15 +58,15 @@ private:
     {
         pathfinding::DijkstraQueue heap;
         heap.emplace(src, 0);
+        distances_[src.get()] = common::Weight{0};
 
         while(!heap.empty()) {
             const auto [current_node, cost_to_current] = heap.top();
             heap.pop();
 
             const auto edge_ids = graph_.getForwardEdgeIDsOf(current_node);
-            const auto current_level = graph_.getNodeLevel(current_node).value_or(common::MAX_LEVEL);
 
-            if(shouldStall(current_level, cost_to_current, edge_ids)) {
+            if(shouldStall(cost_to_current, edge_ids)) {
                 continue;
             }
 
@@ -75,11 +75,6 @@ private:
                 const auto edge = graph_.getEdge(id);
                 const auto neig = edge->getTrg();
                 const auto cost = edge->getWeight();
-                const auto neig_level = graph_.getNodeLevel(neig);
-
-                if(current_level >= neig_level) {
-                    break;
-                }
 
                 const auto new_dist = cost + cost_to_current;
 
@@ -91,20 +86,14 @@ private:
         }
     }
 
-    constexpr auto shouldStall(common::NodeLevel current_level,
-                               common::Weight cost_to_current,
+    constexpr auto shouldStall(common::Weight cost_to_current,
                                const std::span<const common::EdgeID>& edge_ids) const noexcept
         -> bool
     {
-        auto stall_on_demand_valid = false;
         for(const auto id : edge_ids) {
             const auto& edge = graph_.getEdge(id);
             const auto neig = edge->getTrg();
             const auto cost = edge->getWeight();
-
-            if(current_level >= graph_.getNodeLevel(neig)) {
-                return stall_on_demand_valid;
-            }
 
             const auto current_dist_to_neig = distances_[neig.get()];
 
@@ -129,8 +118,15 @@ private:
             const auto trg = edge->getTrg();
             const auto weight = edge->getWeight();
 
-            distances_[trg] = std::min(distances_[trg],
-                                       distances_[src] + weight);
+            const auto src_lvl = graph_.getNodeLevelUnsafe(src);
+            const auto trg_lvl = graph_.getNodeLevelUnsafe(trg);
+
+            fmt::print("src lvl: {}\n", src_lvl.get());
+            fmt::print("trg lvl: {}\n", trg_lvl.get());
+            fmt::print("---------------------------\n");
+
+            distances_[trg.get()] = std::min(distances_[trg.get()],
+                                             distances_[src.get()] + weight);
         }
     }
 
