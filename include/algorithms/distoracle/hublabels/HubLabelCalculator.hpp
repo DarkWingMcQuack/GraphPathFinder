16475301d51c6096ae2538205aa9177d93726b86
lukas <np-hardass@protonmail.com>
2021-10-28 20:19:16 +0200

    implemented the parallel construction of hublabels

--- include/algorithms/distoracle/hublabels/HubLabelCalculator.hpp
+++ include/algorithms/distoracle/hublabels/HubLabelCalculator.hpp
@@ -4,6 +4,7 @@
 #include <algorithms/pathfinding/dijkstra/DijkstraQueue.hpp>
 #include <common/BasicGraphTypes.hpp>
 #include <common/EmptyBase.hpp>
+#include <common/Range.hpp>
 #include <concepts/BackwardConnections.hpp>
 #include <concepts/BackwardEdges.hpp>
 #include <concepts/DistanceOracle.hpp>
@@ -12,6 +13,7 @@
 #include <concepts/NodeLevels.hpp>
 #include <concepts/Nodes.hpp>
 #include <concepts/SortableGraph.hpp>
+#include <execution>
 
 namespace algorithms::distoracle {
 
@@ -28,10 +30,8 @@ template<class Graph>
 class HubLabelCalculator
 {
 public:
-    constexpr HubLabelCalculator(const Graph& graph) noexcept
-        : graph_(graph),
-          in_labels_(graph_.numberOfNodes()),
-          out_labels_(graph_.numberOfNodes()) {}
+    constexpr HubLabelCalculator(const Graph &graph) noexcept
+        : graph_(graph), in_labels_(graph_.numberOfNodes()), out_labels_(graph_.numberOfNodes()) {}
 
 private:
     [[nodiscard]] auto constructOutLabels(common::NodeID node) const noexcept
@@ -40,7 +40,7 @@ private:
         std::vector<HubLabelLookup::HubType> hubs;
         hubs.emplace_back(node, common::Weight{0});
         for(const auto edge_id : graph_.getForwardEdgeIDsOf(node)) {
-            const auto* edge = graph_.getEdge(edge_id);
+            const auto *edge = graph_.getEdge(edge_id);
             const auto trg = edge->getTrg();
 
             const auto weight = [&]() constexpr
@@ -55,32 +55,29 @@ private:
 
             hubs.emplace_back(trg, weight);
 
-            const auto& trg_out_hubs = out_labels_[trg.get()];
-            std::transform(std::begin(trg_out_hubs),
-                           std::end(trg_out_hubs),
-                           std::back_inserter(hubs),
-                           [&](const auto hub) {
+            const auto &trg_out_hubs = out_labels_[trg.get()];
+            std::transform(std::begin(trg_out_hubs), std::end(trg_out_hubs),
+                           std::back_inserter(hubs), [&](const auto hub) {
                                const auto [hub_node, hub_dist] = hub;
                                return std::pair{hub_node, hub_dist + weight};
                            });
         }
 
-        std::sort(std::begin(hubs),
-                  std::end(hubs),
-                  [](const auto& lhs, const auto& rhs) {
-                      return lhs.first < rhs.first;
-                  });
+        std::sort(
+            std::begin(hubs), std::end(hubs),
+            [](const auto &lhs, const auto &rhs) { return lhs.first < rhs.first; });
 
         return hubs;
     }
 
-    constexpr auto pruneOutLabels(common::NodeID node, std::vector<HubLabelLookup::HubType>& hubs) const noexcept
+    constexpr auto
+    pruneOutLabels(common::NodeID node,
+                   std::vector<HubLabelLookup::HubType> &hubs) const noexcept
         -> void
     {
-        hubs.erase(std::unique(hubs.begin(), hubs.end()),
-                   hubs.end());
+        hubs.erase(std::unique(hubs.begin(), hubs.end()), hubs.end());
 
-        std::erase_if(hubs, [&](const auto& hub) {
+        std::erase_if(hubs, [&](const auto &hub) {
             const auto trg = hub.first;
             const auto label_dist = hub.second;
             const auto real_dist = [&] {
@@ -88,7 +85,7 @@ private:
                     return common::Weight{0};
                 }
 
-                const auto& in_hubs = in_labels_[trg.get()];
+                const auto &in_hubs = in_labels_[trg.get()];
                 return HubLabelLookup::distanceOracle(hubs, in_hubs);
             }();
 
@@ -117,32 +114,29 @@ private:
 
             hubs.emplace_back(trg, weight);
 
-            const auto& trg_in_hubs = in_labels_[trg.get()];
-            std::transform(std::begin(trg_in_hubs),
-                           std::end(trg_in_hubs),
-                           std::back_inserter(hubs),
-                           [&](const auto hub) {
+            const auto &trg_in_hubs = in_labels_[trg.get()];
+            std::transform(std::begin(trg_in_hubs), std::end(trg_in_hubs),
+                           std::back_inserter(hubs), [&](const auto hub) {
                                const auto [hub_node, hub_dist] = hub;
                                return std::pair{hub_node, hub_dist + weight};
                            });
         }
 
-        std::sort(std::begin(hubs),
-                  std::end(hubs),
-                  [](const auto& lhs, const auto& rhs) {
-                      return lhs.first < rhs.first;
-                  });
+        std::sort(
+            std::begin(hubs), std::end(hubs),
+            [](const auto &lhs, const auto &rhs) { return lhs.first < rhs.first; });
 
         return hubs;
     }
 
-    constexpr auto pruneInLabels(common::NodeID node, std::vector<HubLabelLookup::HubType>& labels) const noexcept
+    constexpr auto
+    pruneInLabels(common::NodeID node,
+                  std::vector<HubLabelLookup::HubType> &labels) const noexcept
         -> void
     {
-        labels.erase(std::unique(labels.begin(), labels.end()),
-                     labels.end());
+        labels.erase(std::unique(labels.begin(), labels.end()), labels.end());
 
-        std::erase_if(labels, [&](const auto& label) {
+        std::erase_if(labels, [&](const auto &label) {
             const auto trg = label.first;
             const auto label_dist = label.second;
             const auto real_dist = [&] {
@@ -150,12 +144,27 @@ private:
                     return common::Weight{0};
                 }
 
-                const auto& out_hubs = out_labels_[trg.get()];
+                const auto &out_hubs = out_labels_[trg.get()];
                 return HubLabelLookup::distanceOracle(out_hubs, labels);
             }();
 
             return label_dist > real_dist;
         });
+    }
+
+    [[nodiscard]] auto buildMaxNodePerLevelVector() const noexcept
+        -> std::vector<common::NodeID>
+    {
+        const auto max_level = graph_.getNodeLevelUnsafe(common::NodeID{0});
+        std::vector<common::NodeID> max_node_of_level(max_level.get() + 1);
+
+        for(std::size_t i = 0; i < graph_.numberOfNodes(); i++) {
+            const auto node = common::NodeID{i};
+            const auto level = graph_.getNodeLevelUnsafe(node);
+            max_node_of_level[level.get()] = node;
+        }
+
+        return max_node_of_level;
     }
 
 public:
@@ -174,20 +183,48 @@ public:
             out_labels_[i] = std::move(out);
         }
 
+        return HubLabelLookup{std::move(in_labels_), std::move(out_labels_)};
+    }
+
+    [[nodiscard]] auto constructHubLabelLookupInParallel() noexcept
+        -> HubLabelLookup
+    {
+        const auto max_node_of_level = buildMaxNodePerLevelVector();
+        const auto max_level = graph_.getNodeLevelUnsafe(common::NodeID{0}).get();
+        common::NodeID current_node{0};
+
+        for(std::int64_t level = max_level; level >= 0; level--) {
+
+            const auto node_range = common::range(current_node.get(),
+                                                  max_node_of_level[level].get() + 1);
+
+            std::for_each(std::execution::par,
+                          std::begin(node_range),
+                          std::end(node_range),
+                          [&](const auto i) {
+                              common::NodeID node{i};
+
+                              auto in = constructInLabels(node);
+                              pruneInLabels(node, in);
+                              in_labels_[i] = std::move(in);
+
+                              auto out = constructOutLabels(node);
+                              pruneOutLabels(node, out);
+                              out_labels_[i] = std::move(out);
+                          });
+
+            current_node = max_node_of_level[level] + common::NodeID{1};
+        }
+
         return HubLabelLookup{std::move(in_labels_),
                               std::move(out_labels_)};
     }
 
-    [[nodiscard]] auto constructHubLabelsInParallel() noexcept
-        -> HubLabelLookup
-    {}
-
 private:
-    const Graph& graph_;
+    const Graph &graph_;
     std::vector<std::vector<HubLabelLookup::HubType>> in_labels_;
     std::vector<std::vector<HubLabelLookup::HubType>> out_labels_;
 };
-
 
 // clang-format off
 template<class Graph>
@@ -198,9 +235,9 @@ concepts::NodesSortable<Graph>
 [[nodiscard]] inline auto prepareGraphForHubLabelCalculator(Graph g) noexcept
     -> Graph
 {
-    g.deleteForwardEdgesIDsIf([](const auto& graph) {
+    g.deleteForwardEdgesIDsIf([](const auto &graph) {
         return [&](const auto id) {
-            const auto* edge = graph.getEdge(id);
+            const auto *edge = graph.getEdge(id);
             const auto src = edge->getSrc();
             const auto trg = edge->getTrg();
             const auto src_lvl = graph.getNodeLevelUnsafe(src);
@@ -209,8 +246,7 @@ concepts::NodesSortable<Graph>
         };
     });
 
-
-    g.deleteBackwardEdgesIDsIf([](const auto& graph) {
+    g.deleteBackwardEdgesIDsIf([](const auto &graph) {
         return [&](const auto id) {
             const auto edge = graph.getBackwardEdge(id);
             const auto src = edge->getSrc();
@@ -221,7 +257,7 @@ concepts::NodesSortable<Graph>
         };
     });
 
-    g.sortNodesAccordingTo([](const auto& graph) {
+    g.sortNodesAccordingTo([](const auto &graph) {
         return [&](const auto lhs, const auto rhs) {
             const auto lhs_lvl = graph.getNodeLevelUnsafe(lhs);
             const auto rhs_lvl = graph.getNodeLevelUnsafe(rhs);
