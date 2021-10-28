798a74f972106da24b95d3f1678ca1326e62b966
lukas <np-hardass@protonmail.com>
2021-10-28 20:28:59 +0200

    formating

--- include/algorithms/distoracle/hublabels/HubLabelCalculator.hpp
+++ include/algorithms/distoracle/hublabels/HubLabelCalculator.hpp
@@ -31,7 +31,9 @@ class HubLabelCalculator
 {
 public:
     constexpr HubLabelCalculator(const Graph &graph) noexcept
-        : graph_(graph), in_labels_(graph_.numberOfNodes()), out_labels_(graph_.numberOfNodes()) {}
+        : graph_(graph),
+          in_labels_(graph_.numberOfNodes()),
+          out_labels_(graph_.numberOfNodes()) {}
 
 private:
     [[nodiscard]] auto constructOutLabels(common::NodeID node) const noexcept
@@ -56,26 +58,30 @@ private:
             hubs.emplace_back(trg, weight);
 
             const auto &trg_out_hubs = out_labels_[trg.get()];
-            std::transform(std::begin(trg_out_hubs), std::end(trg_out_hubs),
-                           std::back_inserter(hubs), [&](const auto hub) {
+            std::transform(std::begin(trg_out_hubs),
+                           std::end(trg_out_hubs),
+                           std::back_inserter(hubs),
+                           [&](const auto hub) {
                                const auto [hub_node, hub_dist] = hub;
                                return std::pair{hub_node, hub_dist + weight};
                            });
         }
 
-        std::sort(
-            std::begin(hubs), std::end(hubs),
-            [](const auto &lhs, const auto &rhs) { return lhs.first < rhs.first; });
+        std::sort(std::begin(hubs),
+                  std::end(hubs),
+                  [](const auto &lhs, const auto &rhs) {
+                      return lhs.first < rhs.first;
+                  });
 
         return hubs;
     }
 
-    constexpr auto
-    pruneOutLabels(common::NodeID node,
-                   std::vector<HubLabelLookup::HubType> &hubs) const noexcept
+    constexpr auto pruneOutLabels(common::NodeID node,
+                                  std::vector<HubLabelLookup::HubType> &hubs) const noexcept
         -> void
     {
-        hubs.erase(std::unique(hubs.begin(), hubs.end()), hubs.end());
+        hubs.erase(std::unique(hubs.begin(), hubs.end()),
+                   hubs.end());
 
         std::erase_if(hubs, [&](const auto &hub) {
             const auto trg = hub.first;
@@ -115,28 +121,32 @@ private:
             hubs.emplace_back(trg, weight);
 
             const auto &trg_in_hubs = in_labels_[trg.get()];
-            std::transform(std::begin(trg_in_hubs), std::end(trg_in_hubs),
-                           std::back_inserter(hubs), [&](const auto hub) {
+            std::transform(std::begin(trg_in_hubs),
+                           std::end(trg_in_hubs),
+                           std::back_inserter(hubs),
+                           [&](const auto hub) {
                                const auto [hub_node, hub_dist] = hub;
                                return std::pair{hub_node, hub_dist + weight};
                            });
         }
 
-        std::sort(
-            std::begin(hubs), std::end(hubs),
-            [](const auto &lhs, const auto &rhs) { return lhs.first < rhs.first; });
+        std::sort(std::begin(hubs),
+                  std::end(hubs),
+                  [](const auto &lhs, const auto &rhs) {
+                      return lhs.first < rhs.first;
+                  });
 
         return hubs;
     }
 
     constexpr auto
     pruneInLabels(common::NodeID node,
-                  std::vector<HubLabelLookup::HubType> &labels) const noexcept
+                  std::vector<HubLabelLookup::HubType> &hubs) const noexcept
         -> void
     {
-        labels.erase(std::unique(labels.begin(), labels.end()), labels.end());
+        hubs.erase(std::unique(hubs.begin(), hubs.end()), hubs.end());
 
-        std::erase_if(labels, [&](const auto &label) {
+        std::erase_if(hubs, [&](const auto &label) {
             const auto trg = label.first;
             const auto label_dist = label.second;
             const auto real_dist = [&] {
@@ -145,7 +155,7 @@ private:
                 }
 
                 const auto &out_hubs = out_labels_[trg.get()];
-                return HubLabelLookup::distanceOracle(out_hubs, labels);
+                return HubLabelLookup::distanceOracle(out_hubs, hubs);
             }();
 
             return label_dist > real_dist;
