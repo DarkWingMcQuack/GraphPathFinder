29c90fe7ecafb5101a27f38a5eba7f42c47ccc11
lukas <np-hardass@protonmail.com>
2021-10-28 20:24:01 +0200

    added chdijkstra example application

--- examples/chdijkstra.cpp
+++ examples/chdijkstra.cpp
@@ -1,9 +1,7 @@
 #include "utils/Timer.hpp"
 
 #include <CLI/CLI.hpp>
-#include <algorithms/distoracle/hublabels/HubLabelCalculator.hpp>
-#include <algorithms/distoracle/hublabels/HubLabelLookup.hpp>
-#include <fmt/ranges.h>
+#include <algorithms/distoracle/ch/CHDijkstra.hpp>
 #include <graphs/edges/FMIEdge.hpp>
 #include <graphs/nodes/FMINode.hpp>
 #include <graphs/offsetarray/OffsetArray.hpp>
@@ -12,7 +10,7 @@
 
 auto parseCLI(int argc, char* argv[]) -> std::string
 {
-    CLI::App app{"Example calculating and using HubLabels to calculate shortest path distances"};
+    CLI::App app{"Example using contraction hierachies and dijkstra to calculate shortest path distances"};
 
     std::string graph_file;
 
@@ -58,12 +56,8 @@ auto main(int argc, char* argv[])
         std::exit(-1);
     }
 
-    auto graph = std::move(graph_opt.value());
-
-    graph = algorithms::distoracle::prepareGraphForHubLabelCalculator(std::move(graph));
-
-    algorithms::distoracle::HubLabelCalculator calculator{graph};
-    auto hl_lookup = calculator.constructHubLabelLookupInParallel();
+    const auto graph = algorithms::distoracle::prepareGraphForCHDijkstra(std::move(graph_opt.value()));
+    algorithms::distoracle::CHDijkstra dijkstra{graph};
 
     while(true) {
 
@@ -73,7 +67,7 @@ auto main(int argc, char* argv[])
         auto trg = getNumber(graph.numberOfNodes() - 1);
 
         utils::Timer t;
-        auto dist = hl_lookup.distanceBetween(common::NodeID{src}, common::NodeID{trg});
+        auto dist = dijkstra.distanceBetween(common::NodeID{src}, common::NodeID{trg});
         auto time = t.elapsed();
 
         fmt::print("distance: {}\ncalculated in: {}ms", dist.get(), time);
