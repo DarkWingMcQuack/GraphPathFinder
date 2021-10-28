c4155c3dc80b3b5a3c8486cbe534e950aeb987c1
lukas <np-hardass@protonmail.com>
2021-10-28 20:19:31 +0200

    added a test for the parallel construction

--- test/algorithms/distoracle/hublabels/HubLabelTest.cpp
+++ test/algorithms/distoracle/hublabels/HubLabelTest.cpp
@@ -38,3 +38,28 @@ TEST(DistanceOracleHubLabelTest, SequentialHubLabelToyTest)
         }
     }
 }
+
+TEST(DistanceOracleHubLabelTest, ParallelHubLabelToyTest)
+{
+    auto example_graph = data_dir + "ch-fmi-example.txt";
+    auto graph_opt = parsing::parseFromFMIFile<graphs::FMINode<true>, graphs::FMIEdge<true>>(example_graph);
+
+    ASSERT_TRUE(graph_opt);
+    auto graph = std::move(graph_opt.value());
+
+    graph = algorithms::distoracle::prepareGraphForHubLabelCalculator(std::move(graph));
+
+    algorithms::distoracle::HubLabelCalculator calculator{graph};
+    auto hl_lookup = calculator.constructHubLabelLookupInParallel();
+    algorithms::distoracle::CHDijkstra dijkstra{graph};
+
+
+    for(std::size_t i = 0; i < graph.numberOfNodes(); i++) {
+        for(std::size_t j = 0; j < graph.numberOfNodes(); j++) {
+            const auto dij_dist = dijkstra.distanceBetween(common::NodeID{i}, common::NodeID{j});
+            const auto hl_dist = hl_lookup.distanceBetween(common::NodeID{i}, common::NodeID{j});
+
+            EXPECT_EQ(hl_dist, dij_dist);
+        }
+    }
+}
