b859e8763e34a0f9d1f600a0f16e09f84aa8e97a
lukas <np-hardass@protonmail.com>
2021-11-04 18:10:13 +0100

    reordered the benchmarks and use better timing units

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -7,20 +7,20 @@
 #include "phast.hpp"
 
 //parsing
-BENCHMARK(SimpleToyGraphParsing)->Unit(benchmark::kMillisecond);
-BENCHMARK(CHToyGraphParsing)->Unit(benchmark::kMillisecond);
-BENCHMARK(SimpleStgGraphParsing)->Unit(benchmark::kSecond);
-BENCHMARK(CHStgGraphParsing)->Unit(benchmark::kSecond);
 BENCHMARK(FMINodeWithoutLevelParsing);
 BENCHMARK(FMINodeWithLevelParsing);
 BENCHMARK(FMIEdgeWithoutShortcutParsing);
 BENCHMARK(FMIEdgeWithShortcutParsing);
+BENCHMARK(SimpleToyGraphParsing);
+BENCHMARK(CHToyGraphParsing);
+BENCHMARK(SimpleStgGraphParsing)->Unit(benchmark::kSecond);
+BENCHMARK(CHStgGraphParsing)->Unit(benchmark::kSecond);
 
-BENCHMARK(DijkstraInitialization)->Unit(benchmark::kMillisecond);
+BENCHMARK(DijkstraInitialization)->Unit(benchmark::kMicrosecond);
 BENCHMARK(DijkstraOneToOne)->Unit(benchmark::kMillisecond)->Iterations(100);
 BENCHMARK(DijkstraOneToAll)->Unit(benchmark::kMillisecond)->Iterations(50);
 
-BENCHMARK(CHDijkstraGraphPreparation)->Unit(benchmark::kMillisecond)->Iterations(10);
+BENCHMARK(CHDijkstraGraphPreparation)->Unit(benchmark::kMicrosecond)->Iterations(10);
 BENCHMARK(CHDijkstraInitialization)->Unit(benchmark::kMillisecond);
 BENCHMARK(CHDijkstraOneToOne)->Unit(benchmark::kMicrosecond)->Iterations(10000);
 
@@ -29,7 +29,7 @@ BENCHMARK(HubLabelsComputation)->Unit(benchmark::kSecond);
 BENCHMARK(HubLabelsOneToOne)->Unit(benchmark::kMicrosecond);
 
 BENCHMARK(PHASTGraphPreparation)->Unit(benchmark::kMillisecond)->Iterations(10);
-BENCHMARK(PHASTInitialization)->Unit(benchmark::kMillisecond);
+BENCHMARK(PHASTInitialization)->Unit(benchmark::kMicrosecond);
 BENCHMARK(PHASTOneToAll)->Unit(benchmark::kMillisecond)->Iterations(50);
 
 BENCHMARK_MAIN();
