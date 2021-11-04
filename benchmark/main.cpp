e3c067f22fc27fdc94bde2d3dadadcf4d8e69b77
lukas <np-hardass@protonmail.com>
2021-11-04 18:32:01 +0100

    changed the time units of the benchmarks

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -11,8 +11,8 @@ BENCHMARK(FMINodeWithoutLevelParsing);
 BENCHMARK(FMINodeWithLevelParsing);
 BENCHMARK(FMIEdgeWithoutShortcutParsing);
 BENCHMARK(FMIEdgeWithShortcutParsing);
-BENCHMARK(SimpleToyGraphParsing);
-BENCHMARK(CHToyGraphParsing);
+BENCHMARK(SimpleToyGraphParsing)->Unit(benchmark::kMicrosecond);
+BENCHMARK(CHToyGraphParsing)->Unit(benchmark::kMicrosecond);
 BENCHMARK(SimpleStgGraphParsing)->Unit(benchmark::kSecond);
 BENCHMARK(CHStgGraphParsing)->Unit(benchmark::kSecond);
 
@@ -20,7 +20,7 @@ BENCHMARK(DijkstraInitialization)->Unit(benchmark::kMicrosecond);
 BENCHMARK(DijkstraOneToOne)->Unit(benchmark::kMillisecond)->Iterations(100);
 BENCHMARK(DijkstraOneToAll)->Unit(benchmark::kMillisecond)->Iterations(50);
 
-BENCHMARK(CHDijkstraGraphPreparation)->Unit(benchmark::kMicrosecond)->Iterations(10);
+BENCHMARK(CHDijkstraGraphPreparation)->Unit(benchmark::kMillisecond)->Iterations(10);
 BENCHMARK(CHDijkstraInitialization)->Unit(benchmark::kMillisecond);
 BENCHMARK(CHDijkstraOneToOne)->Unit(benchmark::kMicrosecond)->Iterations(10000);
 
